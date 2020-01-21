8befd68757110967f32761f329c565a759d2fd92
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-01-21 14:15:37 +0000

    15. 3Sum

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -2,42 +2,42 @@ from typing import List
 from collections import deque
 from itertools import product
 class Solution:
-    def numIslands(self, grid: List[str]) -> int:
-        if grid == []: return 0
-        S = set()
-        x,y = 0,0
-        Q = deque()
-        land_counter = 0
-        for xx,yy in product(range(len(grid)), range(len(grid[0]))):
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        solution_set = set()
+        couples_set = set()
+        x_set = set()
+        idx = 0
+        D = {}
+        for i in range(len(nums)):
+            D[nums[i]] = D.get(nums[i],0) + 1
+        while idx < len(nums)-2:
+            idy = idx+1
+            x = nums[idx]
+            if x in x_set:
+                idx+=1
+                continue
+            while idy < len(nums)-1:
+                y = nums[idy]
+                if (x,y) in couples_set or (y,x) in couples_set:
+                    idy+=1
+                    continue
+                z = -(x+y)
+                if ((z in D and ((z != x) and (z != y))) or 
+                   (((z==x) and (z==y)) and D.get(z)>2) or
+                   (((z==x) and (z!=y)) and D.get(z)>1) or 
+                   (((z!=x) and (z==y)) and D.get(z)>1)):
+                    sol = tuple(sorted([x,y,z]))
+                    couples_set.add((x,y))
+                    solution_set.add(sol)
+                    x_set.add(x)
+                idy+=1
+            idx+=1
             
-            if (xx,yy) in S: continue # We already visited this cell
-            if grid[xx][yy] == '1': # land!
-                land_counter += 1
-                Q.append((xx,yy))
-                while(Q):
-                    x,y = Q.popleft()
-                    if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]): continue # Out of bounds
-                    if (x,y) in S: continue # If we have already visited this cell
-                    S.add((x,y))            # Visit the cell
-                    if grid[x][y] == '0': continue # If it's water, stop expanding visit
-                    for dx,dy in [(0,1), (1,0), (-1,0), (0,-1)]: # Otherwise we are on a landmass, so keep exploring
-                        Q.append((x+dx, y+dy))
-            else:
-                S.add((xx,yy)) # Just mark the cell as visited
-        return land_counter    
+        return [list(x) for x in solution_set]    
 
+t1 = [-1, 0, 1, 2, -1, -4]
 X = Solution()
-t1 = ['11110',
-      '11010',
-      '11000',
-      '00000'] # One land
+print(X.threeSum(t1))
 
-t2 = ['11000',
-      '11000',
-      '00100',
-      '00011'] # Three lands
-
-print("T1")
-print(X.numIslands(t1))
-print("T2")
-print(X.numIslands(t2))
+t2 = [-2,0,0,2,2]
+print(X.threeSum(t2))
