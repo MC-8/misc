a212d700f52886cee53a8e59099e83857dc6fa64
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-01-22 14:16:52 +0000

    49. group anagrams

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -1,78 +1,24 @@
 from typing import List
+from collections import defaultdict
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        iteration = len(matrix)
-        m = matrix
-        while iteration < n//2:
-            for j in range(iteration, n-1-iteration, 1): #0->3 | 1
-                i = iteration
-                print((i,j))
-                t1 = m[i][j]   # source
-                for _ in range(4):
-                    # print((i,j))
-                    t2 = m[j][n-i-1] # save destination
-                    i, j = j, n-i-1
-                    m[i][j] = t1   # replace
-                    t1 = t2        # source is the saved value
-                print(m)
-            iteration+=1
-        pass
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        D = {}
+        for x in strs:
+            print(x)
+            D.setdefault(''.join(sorted(x)),[]).append(x)
+        return list(D.values())
 
 X = Solution()
 
-m1 = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-]
-# t1 = m1[i][j]
-# t2 = m1[j][n-i]
-# m[j][n-i] = t1
-# i = j
-# j = n-i
-m2 = [
-  [ 5, 1, 9,11],
-  [ 2, 4, 8,10],
-  [13, 3, 6, 7],
-  [15,14,12,16]
-]
-m = m2
-n = 4
-from itertools import product
-
-iteration = 0
-# (0,0), (0,1), (1,1)
-# (0,0), (0,1), (0,2), (0,3), (1,1), (1,2)
-
-# 7 4 1
-# 8 5 2
-# 9 6 3
-# r1c1 -> r1c3
-# r1c2 -> r2c3
-# r1c3 -> r3c3
-
-# r2c1 -> r1c2
-# r2c2 -> r2c2
-# r2c3 -> r3c2
-
-# r3c1 -> r1c1
-# r3c2 -> r2c1
-# r3c3 -> r3c1
-# i, j # row, columns
-
-# i, j -> j, n-i
-
-# X.rotate(m1)
-# print(m1)
-
-# m2 = [
-#   [ 5, 1, 9,11],
-#   [ 2, 4, 8,10],
-#   [13, 3, 6, 7],
-#   [15,14,12,16]
+print(X.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+# Input: 
+# Output:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
 # ]
-# X.rotate(m2)
-# print(m2)
+# 1 create a dictionary where keys are sorted elements, 
+# 2 value is list of entries (unsorted string)
+
+
