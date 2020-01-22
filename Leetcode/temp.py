eff8dbfdae9fafc04e4cfd8d5e728ab97b0b6854
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-01-22 14:50:40 +0000

    55. Jump game

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -1,24 +1,30 @@
-from typing import List
-from collections import defaultdict
+from typing import Counter, List
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        D = {}
-        for x in strs:
-            print(x)
-            D.setdefault(''.join(sorted(x)),[]).append(x)
-        return list(D.values())
+    def canJump(self, nums: List[int]) -> bool:
+        if 0 not in nums: return True
+        if len(nums)<2: return True
+        zflag = False
+        counter = 0
+        #if zflag: counter-=1
+        for x in nums[-2::-1]:
+            if zflag and (x>0 and x>counter):
+                zflag = False
+            if x==0 and not zflag:
+                zflag = True
+            counter = counter + 1 if zflag else 0
+
+        return not zflag
+
 
 X = Solution()
-
-print(X.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
-# Input: 
-# Output:
-# [
-#   ["ate","eat","tea"],
-#   ["nat","tan"],
-#   ["bat"]
-# ]
-# 1 create a dictionary where keys are sorted elements, 
-# 2 value is list of entries (unsorted string)
+print(X.canJump([2,3,1,1,4]))
+print(X.canJump([3,2,1,0,4]))
+print(X.canJump([2,0,0]))
 
 
+
+
+# 1 start from the end
+# If I find one or more zeros, keep looking until you find a value that is N#zeros+1,
+# if reach beginning without finding it return false
+# No zeros, end reachable
