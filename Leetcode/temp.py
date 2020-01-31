4f4b6fc7786018c80cf0a5a69e96308a7d0b2e83
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-01-31 15:18:18 +0000

    146. LRU Cache

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -1,40 +1,40 @@
-from typing import List
-from itertools import product
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        valid = True
-        
-        # Do cells
-        for csr, csc in product(range(0, 7, 3), range(0, 7, 3)):
-            S = set()
-            # Add values
-            num_counter = 0
-            for dr, dc in product(range(3), range(3)):
-                if (val := board[csr+dr][csc+dc]) != ".":
-                    S.add(int(val))
-                    num_counter += 1
-            if len(S)!=num_counter: return False
-            
-        # Do rows
-        for csr in range(9):
-            S = set()
-            # Add values
-            num_counter = 0
-            for dc in range(9):
-                if (val := board[csr][dc]) != ".":
-                    S.add(int(val))
-                    num_counter += 1
-            if len(S)!=num_counter: return False
-        
-        # Do columns
-        for csc in range(9):
-            S = set()
-            # Add values
-            num_counter = 0
-            for dr in range(9):
-                if (val := board[dr][csc]) != ".":
-                    S.add(int(val))
-                    num_counter += 1
-            if len(S)!=num_counter: return False
-        
-        return valid
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.dic          = {}
+        self.capacity   = capacity
+
+    def get(self, key: int) -> int:
+        el = self.dic.pop(key, -1)
+        if el >-1:
+            self.dic[key] = el
+        return el
+
+    def put(self, key: int, value: int) -> None:
+        self.dic.pop(key, -1)
+        self.dic[key] = value
+        if len(self.dic)>self.capacity: 
+            self.dic.pop(next(iter(self.dic.keys())))
+
+from collections import OrderedDict
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+# cache = LRUCache(2)
+# cache.put(1, 1)
+# cache.put(2, 2)
+# print(cache.get(1))
+# cache.put(3, 3)
+# print(cache.get(2))
+# cache.put(4, 4)
+# print(cache.get(1))
+# print(cache.get(3))
+# print(cache.get(4))
+cache = LRUCache(2)
+cache.put(2, 1)
+cache.put(1, 1)
+cache.put(2, 3)
+cache.put(4, 1)
+print(cache.get(1))
+print(cache.get(2))
