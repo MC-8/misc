2787afbd27cb7240201b6406a12c15dccd05c349
Max <massimiliano.curzi@gmail.com>
2020-02-01 20:18:44 +0000

    couples to reach target sum

--- temp.py
+++ temp.py
@@ -1,56 +1,20 @@
-class Solution:
-    def spiralOrder(self, matrix):
-        out_matrix = []
-        M = len(matrix)
-        N = len(matrix[0])
-        
-        # Move right N, N = N-1
-        # move down M-1, M = M-1
-        # move left N, N = N - 1
-        # move up M-1, M = M-1
-        # 1   2   3   4
-        # 10  11  12  5
-        # 9   8   7   6
-        r,c = 0,0
-        
-        c0 = 0
-        cend = N
-        r0 = 0
-        rend = M
+a1 = [-1, 3, 8, 2, 9, 5]
+a2 = [4, 1, 2, 10, 5, 20]
+target = 24
 
-        while len(out_matrix) < M*N:
-            
-            r = r0
-            for c in range(c0, cend):
-                out_matrix.append(matrix[r][c])
-            r0 += 1
-            if len(out_matrix) == M*N: return out_matrix
-            
-            for r in range(r0, rend):
-                out_matrix.append(matrix[r][cend-1])
-            cend -= 1
-            
-            if len(out_matrix) == M*N: return out_matrix
-            r = rend-1
-            for c in range(cend-1, c0-1, -1):
-                out_matrix.append(matrix[r][c])
-            rend -= 1
-            
-            if len(out_matrix) == M*N: return out_matrix
-            c = c0
-            for r in range(rend-1, r0-1, -1):
-                out_matrix.append(matrix[r][c])
-            c0 += 1
-            if len(out_matrix) == M*N: return out_matrix
-            
-        return out_matrix
-        
-X = Solution()
-# print(X.spiralOrder([[1, 2], [3, 4], [5, 6]]))
-s = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9,10,11,12]
-]
+def get_couples(a1, a2, target):
+    s1 = set(a1)
+    s2 = set(a2)
+    result = set()
+    delta = 0
+    while not(result):
+        for el1 in a1:
+            el2 = target - el1
+            if el2+delta in a2:
+                result.add((el1, el2+delta))
+            if el2-delta in a2:
+                result.add((el1, el2-delta))
+        delta += 1
+    return result
 
-print(X.spiralOrder(s))
+print(get_couples(a1, a2, target))
