1bbd2ed87a3afdb47c70725109193359983e2c81
Max <massimiliano.curzi@gmail.com>
2020-02-01 20:48:17 +0000

    338. Counting bits

--- temp.py
+++ temp.py
@@ -1,20 +1,23 @@
-a1 = [-1, 3, 8, 2, 9, 5]
-a2 = [4, 1, 2, 10, 5, 20]
-target = 24
+from typing import List
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        arr = [0]*(num+1)
+        i=1
+        idx = 0
+        ipass = 1
+        while i <= num:
+            arr[i] = arr[idx] + 1
+            i+=1
+            idx+=1
+            if idx>=ipass: 
+                idx = 0
+                ipass *= 2
+        return arr
 
-def get_couples(a1, a2, target):
-    s1 = set(a1)
-    s2 = set(a2)
-    result = set()
-    delta = 0
-    while not(result):
-        for el1 in a1:
-            el2 = target - el1
-            if el2+delta in a2:
-                result.add((el1, el2+delta))
-            if el2-delta in a2:
-                result.add((el1, el2-delta))
-        delta += 1
-    return result
+X = Solution()
+print(X.countBits(2))
+print("---------")
+print(X.countBits(5))
+print("---------")
+print(X.countBits(16))
 
-print(get_couples(a1, a2, target))
