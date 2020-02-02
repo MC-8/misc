e91337b7ba74d7b887d0c8a0bb8e803d3c2ce513
Max <massimiliano.curzi@gmail.com>
2020-02-02 14:46:12 +0000

    find kth largest

--- temp.py
+++ temp.py
@@ -1,16 +1,9 @@
 from typing import List
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        D = {}
-        for n in nums:
-            D[n] = D.get(n,0) + 1
-        res = []
-        for key, _ in sorted(D.items(), key=lambda v:v[1], reverse=True):
-            res.append(key)
-            if len(res)>=k:
-                break
-        return res
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return sorted(nums)[-k]
 
 X = Solution()
-print(X.topKFrequent([3,3,2,3,5,2,7],1))
-print(X.topKFrequent([3,3,2,3,5,2,7],2))
+
+print(X.findKthLargest( [3,2,3,1,2,4,5,5,6],4))
+print(X.findKthLargest( [3,2,3,1,2,4,5,5,6],4))
