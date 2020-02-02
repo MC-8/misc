b9387e6ccf8cfc69ac6e91d179eb4b9b8e5c24de
Max <massimiliano.curzi@gmail.com>
2020-02-02 13:38:05 +0000

    x

--- temp.py
+++ temp.py
@@ -1,23 +1,16 @@
 from typing import List
 class Solution:
-    def countBits(self, num: int) -> List[int]:
-        arr = [0]*(num+1)
-        i=1
-        idx = 0
-        ipass = 1
-        while i <= num:
-            arr[i] = arr[idx] + 1
-            i+=1
-            idx+=1
-            if idx>=ipass: 
-                idx = 0
-                ipass *= 2
-        return arr
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        D = {}
+        for n in nums:
+            D[n] = D.get(n,0) + 1
+        res = []
+        for key, _ in sorted(D.items(), key=lambda v:v[1], reverse=True):
+            res.append(key)
+            if len(res)>=k:
+                break
+        return res
 
 X = Solution()
-print(X.countBits(2))
-print("---------")
-print(X.countBits(5))
-print("---------")
-print(X.countBits(16))
-
+print(X.topKFrequent([3,3,2,3,5,2,7],1))
+print(X.topKFrequent([3,3,2,3,5,2,7],2))
