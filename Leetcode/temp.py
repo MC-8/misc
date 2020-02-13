184e1ba2baa930677b5ddfc2c2899ead1b9e8f8c
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-02-13 10:30:53 +0000

    decode string

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -1,76 +1,44 @@
-from typing import List
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
 class Solution:
-    def rob(self, root: TreeNode) -> int:
-        hMap = {}
-        res = self.robSub(root, hMap)
-        return res
-    
-    def robSub(self, root: TreeNode, hMap: dict):
-        if root == None: return 0
-        if root in hMap: return hMap[root]
+    def decodeString(self, s: str) -> str:
+        # Number k, append k times what comes next until the bracket closing bracket
+        i = 0
         
-        val = 0
-        if root.left:
-            val += self.robSub(root.left.left, hMap) + self.robSub(root.left.right, hMap)
-        if root.right:
-            val += self.robSub(root.right.left, hMap) + self.robSub(root.right.right, hMap)
+        # initial append
+        tmp_str = ''
+        while 'a' <= s[i] <= 'z':
+            tmp_str += s[i]
+            i       += 1
+        
+        num = ''
+        
+        while ('0' <= s[i] <= '9'):
+            num += s[i]
+            i   += 1
+        
+        repeats = int(num)
 
-        val = max(val + root.val, self.robSub(root.left, hMap) + self.robSub(root.right, hMap))
+        substr = ''
+        if s[i]=='[':
+            counter = 1
+            i+=1
+            while s[i] != ']' and counter !=0:
+                if s[i]=='[': 
+                    counter +=  1
+                if s[i] == ']':
+                    counter -= 1
+                substr += s[i]
+                i+=1
+            substr += s[i]
+            
+        print(substr)
         
-        hMap[root] = val
-        
-        return val
-# class Solution:
-#     def rob(self, root: TreeNode) -> int:
-#         res = self.robSub(root)
-#         return max(res[0], res[1])
-    
-#     def robSub(self, root: TreeNode):
-#         if root == None:
-#             return [0,0]
-        
-#         left = self.robSub(root.left)
-#         right = self.robSub(root.right)
-
-#         res = [max(left[0], left[1]) + max(right[0], right[1])]
-#         res.append(root.val + left[0] + right[0])
-        
-#         return res
+        return tmp_str + self.decodeString(substr)*repeats
 
 X = Solution()
 
-t             = TreeNode(3)
-t.left        = TreeNode(2)
-t.right       = TreeNode(3)
-t.left.right  = TreeNode(3)
-t.right.right = TreeNode(1)
-print(X.rob(t))
+print(X.decodeString("3[abcs3[asd]]2[bc]"))
 
-t             = TreeNode(3)
-t.left        = TreeNode(4)
-t.right       = TreeNode(5)
-t.left.left   = TreeNode(1)
-t.left.right  = TreeNode(3)
-t.right.right = TreeNode(1)
-print(X.rob(t))
 
-t                    = TreeNode(1)
-t.left               = TreeNode(0)
-t.right              = TreeNode(0)
-t.left.left          = TreeNode(1)
-t.left.right         = TreeNode(1)
-t.right.left         = TreeNode(0)
-t.right.right        = TreeNode(0)
-t.right.left.left    = TreeNode(1)
-t.right.left.right   = TreeNode(1)
-t.right.right.left   = TreeNode(1)
-t.right.right.right  = TreeNode(1)
 
-print(X.rob(t))
+    
+    
