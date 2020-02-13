007ee78f159b2fdaee3e51985770df36e8dac0f9
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-02-13 11:15:43 +0000

    78. subsets

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -1,44 +1,15 @@
+from typing import List
 class Solution:
-    def decodeString(self, s: str) -> str:
-        # Number k, append k times what comes next until the bracket closing bracket
-        i = 0
-        
-        # initial append
-        tmp_str = ''
-        while 'a' <= s[i] <= 'z':
-            tmp_str += s[i]
-            i       += 1
-        
-        num = ''
-        
-        while ('0' <= s[i] <= '9'):
-            num += s[i]
-            i   += 1
-        
-        repeats = int(num)
-
-        substr = ''
-        if s[i]=='[':
-            counter = 1
-            i+=1
-            while s[i] != ']' and counter !=0:
-                if s[i]=='[': 
-                    counter +=  1
-                if s[i] == ']':
-                    counter -= 1
-                substr += s[i]
-                i+=1
-            substr += s[i]
-            
-        print(substr)
-        
-        return tmp_str + self.decodeString(substr)*repeats
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        sol = []
+        N = len(nums)
+        for idx in range(2**N):
+            sol.append([])
+            for n_bit in range(N):
+                if idx & (1 << n_bit):
+                    sol[idx].append(nums[n_bit])
+        return sol
 
 X = Solution()
 
-print(X.decodeString("3[abcs3[asd]]2[bc]"))
-
-
-
-    
-    
+print(X.subsets([1,2,3]))
