c3dc860418d4981fdd1b05bc800526daf913a351
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-02-13 12:41:27 +0000

    check class variables and instance variables

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -1,15 +1,25 @@
-from typing import List
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        sol = []
-        N = len(nums)
-        for idx in range(2**N):
-            sol.append([])
-            for n_bit in range(N):
-                if idx & (1 << n_bit):
-                    sol[idx].append(nums[n_bit])
-        return sol
+class Shark:
+    fish_type = "Shark"
+    counter = 0
+    
+    def __init__(self) -> None:
+        type(self).counter += 1
+    
+    def __del__(self) -> None:
+        type(self).counter -= 1
 
-X = Solution()
 
-print(X.subsets([1,2,3]))
+S1 = Shark()
+S2 = Shark()
+
+print(S1.fish_type)
+
+S1.fish_type = "Goldfish"
+
+print(S1.fish_type)
+print(S2.fish_type)
+print(Shark.fish_type)
+
+print(S1.counter)
+print(S2.counter)
+print(Shark.counter)
